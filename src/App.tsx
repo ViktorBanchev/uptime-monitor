02833cd1f25aa80ae25697bb54a0a18a99e11b6a
Viktor Banchev <145760312+ViktorBanchev@users.noreply.github.com>
2026-09-23 09:47:42 +0300

    Create Vite project and base file structure
    * initialize npm project
    
    * create vite react project
    
    * Create file structure and cleanup default vite assets

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+	return (
+		<>
+			<h1>Nav will be here</h1>
+
+			<main>
+				Dashboard here
+			</main>
+
+			<h2>Footer here</h2>
+		</>
+	)
+}
+
+export default App
